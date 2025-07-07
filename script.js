78280a3af802f57a7017da78c12915b0083c2ee7
Povilas Dailyda <povilas.dailyda@gmail.com>
2025-07-07 12:29:57 +0300

    time countdown

--- script.js
+++ script.js
@@ -48,50 +48,65 @@ function addZero(t) {
 clock();
 
 let isPomodoro = false;
-let click = 0;
+let click = 0;let countdownInterval; // kad galėtume sustabdyti seną laikmatį
+
 sliderDOM.addEventListener('click', () => {
-    isPomodoro ? isPomodoro = false : isPomodoro = true;
-    sliderDOM.classList.toggle('active')
-   
-    isPomodoro ? work() : remove()
-    click++
-    
-    if (click % 2 === 1) {
-        pDOM.insertAdjacentHTML('beforeend', `<h3 style="color:green">Šiuo metu 25min vyks darbas</h3>
-              
-    <div id="timer">25:00</div>
-    `)
-        
-    }
-    else {
-        pDOM.innerHTML = '';
-        pDOM.insertAdjacentHTML('beforeend', `<h3 style="color:blue">Šiuo metu vyksta 5min petrauka</h3>
-           
-    <div id="timer">05:00</div>
-   `)
-        
-        
-    }
+    isPomodoro = !isPomodoro;
+    sliderDOM.classList.toggle('active');
+    click++;
 
+    // išvalyti seną laikmatį
+    clearInterval(countdownInterval);
 
-})
+    if (isPomodoro) {
+        pDOM.innerHTML = `<h3 style="color:green">Šiuo metu 25min vyks darbas</h3>
+        <div id="timer">25:00</div>`;
+        startCountdown(25 * 60, () => { // 25 min
+            chill(); // kai baigiasi darbas, pradedam pertrauką
+        });
+    } else {
+        pDOM.innerHTML = `<h3 style="color:blue">Šiuo metu vyksta 5min pertrauka</h3>
+        <div id="timer">05:00</div>`;
+        startCountdown(5 * 60, () => { // 5 min
+            work(); // kai baigiasi pertrauka, pradedam darbą
+        });
+    }
+});
+
+function startCountdown(duration, onEnd) {
+    let timer = duration;
+    const timerEl = document.getElementById('timer');
+
+    countdownInterval = setInterval(() => {
+        let minutes = Math.floor(timer / 60);
+        let seconds = timer % 60;
+
+        minutes = addZero(minutes);
+        seconds = addZero(seconds);
+
+        timerEl.textContent = `${minutes}:${seconds}`;
+
+        if (--timer < 0) {
+            clearInterval(countdownInterval);
+            onEnd();
+        }
+    }, 1000);
+}
+
 function chill() {
-    timeElDOM.classList.remove('chill')
-    timeElDOM.classList.add('work')
-    isPomodoro ? setTimeout(work, 5 * 60000) : remove()
-    pDOM.innerHTML = ''; // Išvalo seną pranešimą
-
+    timeElDOM.classList.remove('chill');
+    timeElDOM.classList.add('work');
+    pDOM.innerHTML = `<h3 style="color:blue">Šiuo metu vyksta 5min pertrauka</h3>
+    <div id="timer">05:00</div>`;
+    startCountdown(5 * 60, work);
 }
 
 function work() {
-    
-    timeElDOM.classList.remove('work')
-    timeElDOM.classList.add('chill')
-    isPomodoro ? setTimeout(chill, 25 * 60000) : remove()
-    pDOM.innerHTML = ''; // Išvalo seną pranešimą
-
-
-
+    timeElDOM.classList.remove('work');
+    timeElDOM.classList.add('chill');
+    pDOM.innerHTML = `<h3 style="color:green">Šiuo metu 25min vyks darbas</h3>
+    <div id="timer">25:00</div>`;
+    startCountdown(25 * 60, chill);
 }
 
 function remove() {
